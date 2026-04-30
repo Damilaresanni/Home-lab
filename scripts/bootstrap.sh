c3a46fd75ab2b625194177ae831ae2820530b324
damilaresanni <sannidamilare9@gmail.com>
2026-04-30 07:18:24 +0000

    chore(script): change Ohmyzsh download command to last on  bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,34 +2,35 @@
 
 set -euo pipefail
 
-echo "==> [1/6] updating system packages..."
+echo "==> [1/7] updating system packages..."
 sudo apt update && sudo apt update -y
 
-echo "==> [2/6] Installing base tools..."
+echo "==> [2/7] Installing base tools..."
 sudo apt install -y \
     curl git vim htop tmux \
     
-echo "==> [3/6] Installing ohmyzsh..."
+echo "==> [3/6] Installing zsh..."
 sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "==> [4/6] configuring firewall..."
+echo "==> [4/7] configuring firewall..."
 sudo ufw allow 6443/tcp 
 sudo ufw allow from 10.42.0.0/16 to any 
 sudo ufw allow from 10.43.0.0/16 to any 
 
 
 
-echo "==> [5/6] installing Docker..."
+echo "==> [5/7] installing Docker..."
 curl -fsSL https://get.docker.com | sh 
 sudo usermod -aG docker $usermod
 
 
-echo "==> [6/6] Installing Tailscale..."
+echo "==> [6/7] Installing Tailscale..."
 curl -fsSL https:://tailscale.com/install.sh | sh
 echo " --> Run 'sudo tailscale up' to authenticate tailscale on your telnet network"
 
 
+echo "==> [7/7] Installing Ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
 echo ""
